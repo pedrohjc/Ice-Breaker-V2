52d2548efecce4f533f3fe6754a20a6e2f23f78b
pedrohjc <pedrohj.carvalho@gmail.com>
2018-12-18 13:09:48 -0500

    Initial Commit and Starting Point

---
+++ app/src/main/java/com/example/android/ice_breaker_v2/LoginActivity.java
@@ -0,0 +1,28 @@
+package com.example.android.ice_breaker_v2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    Button forgot_pwd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        forgot_pwd = (Button) findViewById(R.id.forgot_pwd);
+        forgot_pwd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, ListActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
