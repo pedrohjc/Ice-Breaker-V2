3dfbd5893d5345b0dc9c909dc3ad253dd7dc3fc3
pedrohjc <pedrohj.carvalho@gmail.com>
2019-01-22 14:42:08 -0500

    ADD button with AlertBuilder

--- app/src/main/java/com/example/android/ice_breaker_v2/User.java
+++ app/src/main/java/com/example/android/ice_breaker_v2/User.java
@@ -1,4 +1,56 @@
 package com.example.android.ice_breaker_v2;
 
 public class User {
+
+    private int mUser_img = NO_IMAGE;
+    private String mUser_fname, mUser_lname;
+    private static final int NO_IMAGE = -1;
+    private int mUser_age;
+    private String mUser_city;
+
+    public User(String fn, String ln, int age, String city){
+        mUser_age = age;
+        mUser_city = city;
+        mUser_lname = ln;
+        mUser_fname = fn;
+    }
+
+    public User(String fn, int age, String city){
+        mUser_age = age;
+        mUser_city = city;
+        mUser_fname = fn;
+    }
+
+
+    public User(String fn, String ln, int age, String city, int image){
+        mUser_age = age;
+        mUser_city = city;
+        mUser_lname = ln;
+        mUser_fname = fn;
+        mUser_img = image;
+    }
+
+    public int getmUser_img() {
+        return mUser_img;
+    }
+
+    public String getmUser_fname() {
+        return mUser_fname;
+    }
+
+    public String getmUser_lname() {
+        return mUser_lname;
+    }
+
+    public int getmUser_age() {
+        return mUser_age;
+    }
+
+    public String getmUser_city() {
+        return mUser_city;
+    }
+
+    public boolean hasImage(){
+        return mUser_img != NO_IMAGE;
+    }
 }
--- app/src/main/java/com/example/android/ice_breaker_v2/UsersList.java
+++ app/src/main/java/com/example/android/ice_breaker_v2/UsersList.java
@@ -1,16 +1,65 @@
 package com.example.android.ice_breaker_v2;
 
 import android.app.ActionBar;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class UsersList extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_users_list);
+
+        final ArrayList<User> userList = new ArrayList<User>();
+        Button add = (Button) findViewById(R.id.add_btn);
+
+        userList.add(new User("Pedro","Carvalho",28,"Toronto"));
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(UsersList.this);
+                builder.setTitle("Add new User");
+                final EditText last = new EditText(UsersList.this);
+                builder.setView(last);
+//                final EditText first = new EditText(UsersList.this);
+//                builder.setView(first);
+//                final EditText city = new EditText(UsersList.this);
+//                builder.setView(city);
+//                final EditText age = new EditText(UsersList.this);
+//                builder.setView(age);
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String ln = last.getText().toString();
+//                        Toast.makeText(getApplicationContext(), "OK button Pressed", Toast.LENGTH_LONG).show();
+                        Toast.makeText(getApplicationContext(), "last name:" + ln, Toast.LENGTH_LONG).show();
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getApplicationContext(), "CANCEL button Pressed", Toast.LENGTH_LONG).show();
+                    }
+                });
+                Toast.makeText(getApplicationContext(), "Worked bitches!", Toast.LENGTH_LONG).show();
+                builder.show();
+            }
+        });
     }
+
 }
+
+
